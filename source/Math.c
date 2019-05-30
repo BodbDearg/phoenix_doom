388776cf4fb125ab951315fee3a5b8a558d4e33c
Darragh Coy <darraghcoy@runbox.com>
2019-05-30 21:49:02 +0100

    Fix some errors with missing fixed point functions

---
+++ source/Math.c
@@ -0,0 +1,13 @@
+#include "Math.h"
+
+#include <intmath.h>
+
+// DC: TODO: Remove - this is a temp fix for not having this old assembly function
+Fixed IMFixDiv(Fixed a, Fixed b) {
+    return sfixedDiv16_16(a, b);
+}
+
+// DC: TODO: Remove - this is a temp fix for not having this old assembly function
+Fixed IMFixMul(Fixed a, Fixed b) {
+    return sfixedMul16_16(a, b);
+}
---
+++ source/Math.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdint.h>
+
+// TODO: test
+static inline int32_t sfixedMul16_16(int32_t num1, int32_t num2) {
+    int64_t result64 = (int64_t) num1 * (int64_t) num2;
+    return (int32_t) (result64 >> 16);
+}
+
+// TODO: test
+static inline int32_t sfixedDiv16_16(int32_t num1, int32_t num2) {
+    int64_t result64 = (((int64_t) num1) << 16) / (int64_t) num2;
+    return (int32_t) result64;
+}
+
+// TODO: test
+static inline int32_t floatToSFixed16_16(float num) {
+    double doubleVal = (double) num * 65536.0;
+    return (int32_t) doubleVal;
+}
+
+// TODO: test
+static inline int32_t sfixed16_16ToFloat(int32_t num) {
+    double doubleVal = (double) num;
+    return (float) (doubleVal / 65536.0);
+}
