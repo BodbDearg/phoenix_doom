e2b234ce31d4541c0e07c9752cd8fbe89d67f7e2
darraghcoy@runbox.com <darraghcoy@runbox.com>
2019-06-21 23:35:53 -0700

    CMake files: doing away with separation of header + source

--- cmake/HelperFunctions.cmake
+++ cmake/HelperFunctions.cmake
@@ -2,21 +2,17 @@
 # Setup grouping/folders for the current target.
 # Useful to group source and other files in IDEs like Visual Studio.
 #-----------------------------------------------------------------------------------------------------------------------
-function(setup_source_groups SOURCE_FILES HEADER_FILES OTHER_FILES)
+function(setup_source_groups SOURCE_FILES OTHER_FILES)
     # CMakeLists.txt is special - put it at the root of the project for easy access
     set(OTHER_FILES_MINUS_CMAKE_LISTS "${OTHER_FILES}")
     list(REMOVE_ITEM OTHER_FILES_MINUS_CMAKE_LISTS "CMakeLists.txt")
     source_group(TREE "${CMAKE_CURRENT_LIST_DIR}" PREFIX "" FILES "CMakeLists.txt")
-
+    
     # Regular source and other files
     if (SOURCE_FILES)
         source_group(TREE "${CMAKE_CURRENT_LIST_DIR}" PREFIX "Source Files" FILES ${SOURCE_FILES})
     endif()
-
-    if (HEADER_FILES)
-        source_group(TREE "${CMAKE_CURRENT_LIST_DIR}" PREFIX "Header Files" FILES ${HEADER_FILES})
-    endif()
-
+    
     if (OTHER_FILES_MINUS_CMAKE_LISTS)
         source_group(TREE "${CMAKE_CURRENT_LIST_DIR}" PREFIX "Other Files" FILES ${OTHER_FILES_MINUS_CMAKE_LISTS})
     endif()
