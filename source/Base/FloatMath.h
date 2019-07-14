27ff225cf22678463005be952b5470cce91bc0b4
Darragh Coy <darraghcoy@runbox.com>
2019-07-13 23:16:49 -0700

    Conversions between Doom 16.16 fixed and float

--- source/Base/FloatMath.h
+++ source/Base/FloatMath.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Angle.h"
+#include "Fixed.h"
 #include <cmath>
 
 namespace FloatMath {
@@ -18,7 +19,7 @@ namespace FloatMath {
     template <class T> static constexpr T ANGLE_1   = ANGLE_180<T> / T(180.0);
     
     //------------------------------------------------------------------------------------------------------------------
-    // Convert a Doom binary angle to a float angle (in radians)
+    // Convert a Doom binary angle to a float angle (in radians) and back again
     //------------------------------------------------------------------------------------------------------------------
     template <class T>
     inline T doomAngleToRadians(const angle_t angle) noexcept {
@@ -27,14 +28,24 @@ namespace FloatMath {
         return T(twoPiRange);
     }
     
-    //------------------------------------------------------------------------------------------------------------------
-    // Convert a float angle (in radians) to a Doom binary angle
-    //------------------------------------------------------------------------------------------------------------------
     template <class T>
     inline angle_t radiansToDoomAngle(const T angle) noexcept {
         const double twoPiRange = std::fmod(double(angle), ANGLE_360<double>);
         const double normalized = twoPiRange / ANGLE_360<double>;
         const double intRange = normalized * (double(UINT32_MAX) + 1.0);
         return angle_t(intRange);
+    }
+    
+    //------------------------------------------------------------------------------------------------------------------
+    // Convert a Doom 16.16 fixed point number to float and back again
+    //------------------------------------------------------------------------------------------------------------------
+    template <class T>
+    inline T doomFixed16ToFloat(const Fixed fixed) noexcept {
+        return T((double) fixed * (1.0 / 65536.0));
+    }
+    
+    template <class T>
+    inline Fixed floatToDoomFixed16(const T value) noexcept {
+        return Fixed((double) value * 65536.0);
     }
 }
