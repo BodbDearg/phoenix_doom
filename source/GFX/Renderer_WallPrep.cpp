485b96e71ad5c2e4746ca261a218a24bd10d347a
darraghcoy@runbox.com <darraghcoy@runbox.com>
2019-07-23 01:10:50 -0700

    Getting closer to having texture mapping in the horz direction working

--- source/GFX/Renderer_WallPrep.cpp
+++ source/GFX/Renderer_WallPrep.cpp
@@ -666,10 +666,12 @@ static void emitWallAndFloorFragments(const DrawSeg& drawSeg, const seg_t seg) n
         // This is the texture to use
         const Texture* pTexture;
 
-        if constexpr (MODE == EmitFragmentMode::LOWER_WALL) {
-            pTexture = getWallTexture(seg.sidedef->bottomtexture);
-        } else {
+        if constexpr (MODE == EmitFragmentMode::SOLID_WALL) {
+            pTexture = getWallTexture(seg.sidedef->midtexture);
+        } else if constexpr (MODE == EmitFragmentMode::UPPER_WALL) {
             pTexture = getWallTexture(seg.sidedef->toptexture);
+        } else {
+            pTexture = getWallTexture(seg.sidedef->bottomtexture);
         }
 
         // Figure out the start 'V' texture coordiante for the column
@@ -734,18 +736,30 @@ static void emitWallAndFloorFragments(const DrawSeg& drawSeg, const seg_t seg) n
         float worldH = worldTZ - worldBZ;
         float texBV = texTV + worldH - 0.001f;  // Note: moving down a little so we don't skip over the end pixel bounds
 
+        // Figure out the u and w coordinates for both wall ends and the u step
+        const float p1w = drawSeg.coords.p1w;
+        const float p2w = drawSeg.coords.p2w;
+        const float p1TexU = drawSeg.p1TexU / p1w;
+        const float p2TexU = drawSeg.p2TexU / p2w;
+
         // Figure out how much to step in the y direction for each x pixel
         const uint32_t colCount = (x2 - x1) + 1;
         float ztStep;
         float zbStep;
+        float wStep;
+        float texUStep;
 
         if (colCount > 1) {
             const float horzStepDivider = 1.0f / (float) (colCount - 1);
             ztStep = (p2tz - p1tz) * horzStepDivider;
             zbStep = (p2bz - p1bz) * horzStepDivider;
+            wStep = (p2w - p1w) * horzStepDivider;
+            texUStep = (p2TexU - p1TexU) * horzStepDivider;
         } else {
             ztStep = 0.0f;
             zbStep = 0.0f;
+            wStep = 0.0f;
+            texUStep = 0.0;
         }
 
         // Emit each column
@@ -753,10 +767,12 @@ static void emitWallAndFloorFragments(const DrawSeg& drawSeg, const seg_t seg) n
         const float bottomClipZ = viewH - 0.5f;
 
         for (int32_t x = x1; x <= x2; ++x) {
-            // Figure out the top and bottom y through linear interpolation
+            // Figure out the top and bottom y, w and u through linear interpolation
             const float pixelNumF = (float) (x - x1);
             float zt = viewH - (p1tz + ztStep * pixelNumF);
             float zb = viewH - (p1bz + zbStep * pixelNumF);
+            const float w = p1w + wStep * pixelNumF;
+            const float texU = (p2TexU + texUStep * pixelNumF) * w;
 
             // This is how much to step in the V direction
             const float texVStep = (texTV - texBV) / (zb - zt);
@@ -797,11 +813,12 @@ static void emitWallAndFloorFragments(const DrawSeg& drawSeg, const seg_t seg) n
 
             Blit::blitColumn<
                 Blit::BCF_STEP_Y |
+                Blit::BCF_H_WRAP_WRAP |
                 Blit::BCF_V_WRAP_WRAP |
                 Blit::BCF_COLOR_MULT_RGB
             >(
                 pTexture->data,
-                0.0f,  // TODO: texture U coordinate
+                texU,
                 curTexTV,
                 Video::gFrameBuffer,
                 Video::SCREEN_WIDTH,
